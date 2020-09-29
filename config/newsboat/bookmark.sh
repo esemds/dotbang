c5569b5a8529e88d1cb453ff1ee674187069cb38
HASAN esemds <esemdshmz@gmail.com>
2020-09-29 22:27:46 +0330

    added newsboat :)

---
+++ config/newsboat/bookmark.sh
@@ -0,0 +1,11 @@
+# t 1
+if [ -n $(command -v curl) ]; then
+  url=$(curl -sIL -o /dev/null -w '%{url_effective}' "$1")
+else  
+  url="$1"
+fi
+url=$(echo "${url}" | perl -p -e 's/(\?|\&)?utm_[a-z]+=[^\&]+//g;' -e 's/(#|\&)?utm_[a-z]+=[^\&]+//g;')
+title="$2"
+description="$3"
+
+grep -q "${url}\t${title}\t${description}" ~/.newsboat/bookmarks.txt || echo "${url} [${title} _-_ ${description}]" >> ~/.newsboat/bookmarks.txt 
